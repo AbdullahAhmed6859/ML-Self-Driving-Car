db6dae8f94e71a059403840753e070841f3a9339
AbdullahAhmed6859 <109270476+AbdullahAhmed6859@users.noreply.github.com>
2023-07-21 12:00:30 +0500

    Project Complete

---
+++ main.js
@@ -0,0 +1,97 @@
+const mK = 0.1;
+const N = 500;
+
+let justLanded = true;
+
+document.getElementById("gen").textContent =
+  "Generation:" + localStorage.getItem("gen");
+
+const carCanvas = document.getElementById("carCanvas");
+carCanvas.width = 200;
+const networkCanvas = document.getElementById("networkCanvas");
+networkCanvas.width = 400;
+
+const carCtx = carCanvas.getContext("2d");
+const networkCtx = networkCanvas.getContext("2d");
+
+const road = new Road(carCanvas.width / 2, carCanvas.width * 0.9);
+
+const cars = generateCars(N);
+let bestCar = cars[0];
+
+if (localStorage.getItem("bestBrain")) {
+  for (let i = 0; i < cars.length; i++) {
+    cars[i].brain = JSON.parse(localStorage.getItem("bestBrain"));
+
+    if (i != 0) {
+      NeuralNetwork.mutate(cars[i].brain, mK);
+    }
+  }
+}
+
+const traffic = [
+  new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY", 2),
+  new Car(road.getLaneCenter(0), -300, 30, 50, "DUMMY", 2),
+  new Car(road.getLaneCenter(2), -300, 30, 50, "DUMMY", 2),
+  new Car(road.getLaneCenter(0), -500, 30, 50, "DUMMY", 2),
+  new Car(road.getLaneCenter(1), -500, 30, 50, "DUMMY", 2),
+  new Car(road.getLaneCenter(1), -700, 30, 50, "DUMMY", 2),
+  new Car(road.getLaneCenter(2), -700, 30, 50, "DUMMY", 2),
+  new Car(road.getLaneCenter(0), -900, 30, 50, "DUMMY", 2),
+  new Car(road.getLaneCenter(2), -900, 30, 50, "DUMMY", 2),
+];
+
+animate();
+
+function save() {
+  localStorage.setItem("bestBrain", JSON.stringify(bestCar.brain));
+  if (justLanded && N !== 1)
+    localStorage.setItem("gen", Number(localStorage.getItem("gen")) + 1);
+  justLanded = false;
+}
+
+function discard() {
+  localStorage.removeItem("bestBrain");
+  localStorage.setItem("gen", 1);
+}
+
+function generateCars(N) {
+  const cars = [];
+  for (let i = 0; i < N; i++) {
+    cars.push(new Car(road.getLaneCenter(1), 100, 30, 50, "AI"));
+  }
+  return cars;
+}
+
+function animate(time) {
+  for (let i = 0; i < traffic.length; i++) {
+    traffic[i].update(road.borders, []);
+  }
+
+  cars.forEach((car) => car.update(road.borders, traffic));
+
+  bestCar = cars.find((car) => car.y === Math.min(...cars.map((c) => c.y)));
+
+  carCanvas.height = window.innerHeight;
+  networkCanvas.height = window.innerHeight;
+
+  carCtx.save();
+  carCtx.translate(0, -bestCar.y + carCanvas.height * 0.7);
+
+  road.draw(carCtx);
+  traffic.forEach((car) => car.draw(carCtx, "red"));
+
+  carCtx.globalAlpha = 0.3;
+  for (let i = 0; i < cars.length; i++) {
+    cars[i].draw(carCtx, "blue");
+  }
+  carCtx.globalAlpha = 1;
+
+  bestCar.draw(carCtx, "blue", true);
+
+  carCtx.restore();
+
+  networkCtx.lineDashOffset = -time / 50;
+  Visualizer.drawNetwork(networkCtx, bestCar.brain);
+  requestAnimationFrame(animate);
+}
